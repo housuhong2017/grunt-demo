65201ba0ea5b6b9f535c1005389a2e2095adc048
wangding <408542507@qq.com>
2019-03-12 16:33:24 +0800

    uglimy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,20 @@
-module.exports = function(grunt){
-      grunt.initConfig({
-      uglify: {
-        release: {
-          files: {
-                    'dist/rectangle.js': ['./rectangle.js']
-                   // 'dist/calc.js':['./calc.js']
+module.exports = function (grunt) {
+  grunt.initConfig({
+    uglify: {
+      release:{
+        files: {
+                    './rectangle.js': 'js/rectangle.js'
             
-          }
-                     
         }
                      
-      }
+      }       
+                  
+    }
+              
+  });
 
-    });
- }
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.registerTask('build', ['uglify']);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+      grunt.registerTask('default', ['uglify:release']);
+
+};
